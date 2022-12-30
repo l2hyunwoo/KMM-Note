64d8b3eb07a1594602da312379a64429ee08b548
HyunWoo Lee <l2hyunwoo@gmail.com>
2022-12-30 18:22:49 +0900

    CRUD에 필요한 Mapper, 추가적인 Util, 데이터 타입 추가

---
+++ shared/src/commonMain/kotlin/com/github/hyunwoo/note/data/NoteMapper.kt
@@ -0,0 +1,17 @@
+package com.github.hyunwoo.note.data
+
+import com.github.hyunwoo.note.domain.note.Note
+import database.NoteEntity
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+
+fun NoteEntity.toNote() = Note(
+    id = id,
+    title = title,
+    content = content,
+    colorHex = colorHex,
+    created = Instant
+        .fromEpochMilliseconds(created)
+        .toLocalDateTime(TimeZone.currentSystemDefault())
+)
---
+++ shared/src/commonMain/kotlin/com/github/hyunwoo/note/presentation/Colors.kt
@@ -0,0 +1,7 @@
+package com.github.hyunwoo.note.presentation
+
+const val RedOrange = 0xffffab91
+const val RedPink = 0xfff48fb1
+const val BabyBlue = 0xff81deea
+const val Violet = 0xffcf94da
+const val LightGreen = 0xffe7ed9b
